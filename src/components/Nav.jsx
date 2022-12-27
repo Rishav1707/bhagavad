93e380aa9f6b73ecc047f0802255a551575a80be
ZZCWC <85448514+losier@users.noreply.github.com>
2022-12-27 16:06:29 +0530

    added some css and nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import '../styles/nav.css'
+
+import TypingEffect from './TypingEffect';
 
 const Nav = () => {
+  const [show, setShow] = useState(() => null);
+
+  useEffect(() => {
+    let lastScrollPosition = 0;
+    setShow(true);
+    document.addEventListener(
+      "scroll",
+      function (e) {
+        if (window.scrollY > lastScrollPosition) {
+          setShow(false);
+        } else {
+          setShow(true);
+        }
+        lastScrollPosition = window.scrollY;
+      },
+      {
+        passive: true,
+      }
+    );
+  }, []);
+
   return (
-    <nav>
+    <nav
+      style={{
+        opacity: show ? 1 : 0,
+        top: show ? 0 : "-100px",
+      }}
+    >
+      <div className="nav-text">
+        <h1>{<TypingEffect />}</h1>
+      </div>
     </nav>
   );
 }
